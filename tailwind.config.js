3544e3e7c6b46ad8df88b2cd29887932ed591629
kazuyuki-a-dev <kazuyuki.a.dev@gmail.com>
2026-09-26 13:46:00 +0900

    style: 色と文字の土台を登録して、画面の枠に反映する (#9)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['"BIZ UDPGothic"', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                sea: {
+                    50: "#E8F0F2",
+                    100: "#CFE0E4",
+                    400: "#3F7486",
+                    500: "#1C5569",
+                    600: "#12465A",
+                    700: "#0D3747",
+                    DEFAULT: "#12465A",
+                },
+                tide: "#EEF3F2",
+                float: {
+                    DEFAULT: "#E0572B",
+                    dark: "#C2461F",
+                },
+                ink: "#1D2A30",
+                sand: "#8A9A9F",
             },
         },
     },
